6c41d9ce9abe576502c8eb0d12f64db3c57a8261
RyanTHSTGS <ryschwaghy@gmail.com>
2024-07-31 13:45:55 -0700

    Added Project 0

---
+++ SpringTeamManager/SpringTeamManager/src/main/java/Model/Team.java
@@ -0,0 +1,62 @@
+package Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "teams")
+
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id", updatable = false)
+    private int id;
+
+    @Setter
+    @Getter
+    @Column(name = "team_name", nullable = false, unique = true)
+    private String team_name;
+
+    @Setter
+    @Getter
+    @Column(name = "wins", columnDefinition = "int CHECK (wins >= 0)")
+    private int wins;
+
+    @Setter
+    @Getter
+    @Column(name = "loses", columnDefinition = "int CHECK (loses >= 0)")
+    private int loses;
+
+    public Team(String team_name, int wins, int loses) {
+        this.team_name = team_name;
+        this.wins = wins;
+        this.loses = loses;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "team_name='" + team_name + '\'' +
+                ", wins=" + wins +
+                ", loses=" + loses +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Team team = (Team) o;
+        return id == team.id && getWins() == team.getWins() && getLoses() == team.getLoses() && Objects.equals(getTeam_name(), team.getTeam_name());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, getTeam_name(), getWins(), getLoses());
+    }
+}
+
---
+++ SpringTeamManager/SpringTeamManager/src/test/java/com/revature/SpringTeamManager/SpringTeamManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.SpringTeamManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTeamManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
