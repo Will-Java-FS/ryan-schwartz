535e2b739792ee12a7fe26b114f63005826dfc16
BolaGhaly <bolaghaly63@gmail.com>
2024-08-02 14:06:16 -0400

    Update user model to get user_id

--- SpringTeamManager/SpringTeamManager/src/main/java/com/revature/Model/User.java
+++ SpringTeamManager/SpringTeamManager/src/main/java/com/revature/Model/User.java
@@ -18,6 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", updatable = false)
+    @Getter
     private int user_id;
 
     @Setter
