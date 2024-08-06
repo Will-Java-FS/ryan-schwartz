09738b2bbc41dd1d6b1f049375d36b05d97aff39
BolaGhaly <bolaghaly63@gmail.com>
2024-08-06 19:51:28 -0400

    Update teams endpoints in teams controller

--- SpringTeamManager/SpringTeamManager/src/main/java/com/revature/Controller/TeamController.java
+++ SpringTeamManager/SpringTeamManager/src/main/java/com/revature/Controller/TeamController.java
@@ -1,7 +1,7 @@
 package com.revature.Controller;
 
 import com.revature.Model.Team;
-import com.revature.Model.User;
+import com.revature.Repos.TeamsRepo;
 import com.revature.Services.TeamServ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,18 +9,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teams")
 @CrossOrigin
 public class TeamController {
     TeamServ ts;
+    TeamsRepo tr;
 
     @Autowired
-    public TeamController(TeamServ ts) {
+    public TeamController(TeamServ ts, TeamsRepo tr) {
         this.ts = ts;
+        this.tr = tr;
     }
 
     @GetMapping("/secret")
@@ -35,30 +37,44 @@ public class TeamController {
 
     @GetMapping("/{teamId}")
     public ResponseEntity<Object> getTeam(@PathVariable int teamId) {
-        return ResponseEntity.status(200).header("content-type", "application/json").body(ts.getTeam(teamId));
+        Optional<Team> isTeamInDB = Optional.ofNullable(tr.findByTeamId(teamId));
+        Map<String, String> message = new HashMap<String, String>();
+
+        if (isTeamInDB.isEmpty()) {
+            message.put("Error", "Team does not exist in the database (incorrect team id)!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).header("content-type", "application/json").body(message);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).header("content-type", "application/json").body(ts.getTeam(teamId));
+        }
     }
 
     @GetMapping("/name/{teamName}")
     public ResponseEntity<Object> getTeam(@PathVariable String teamName) {
-        return ResponseEntity.status(200).header("content-type", "application/json").body(ts.getTeam(teamName));
+        Optional<Team> isTeamInDB = Optional.ofNullable(tr.findByName(teamName));
+        Map<String, String> message = new HashMap<String, String>();
+
+        if (isTeamInDB.isEmpty()) {
+            message.put("Error", "Team does not exist in the database (incorrect team name)!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).header("content-type", "application/json").body(message);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).header("content-type", "application/json").body(ts.getTeam(teamName));
+        }
     }
 
     @DeleteMapping("/{teamId}")
     public ResponseEntity<Object> deleteTeam(@PathVariable int teamId) {
         Map<String, String> message = new HashMap<String, String>();
-        if(ts.getTeam(teamId) == null)
-        {
-            message.put("Error", "Team at " + teamId + " doesn't exist");
+
+        if (ts.getTeam(teamId) == null) {
+            message.put("Error", "Team at " + teamId + " doesn't exist.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
         }
-        if( ts.deleteTeam(teamId))
-        {
-            message.put("Message", "Team at " + teamId + " deleted successfully");
-            return ResponseEntity.status(200).header("content-type", "application/json").body(message);
-        }
-        else
-        {
-            message.put("Error", "Team at " + teamId + " couldn't be deleted");
+
+        if (ts.deleteTeam(teamId)) {
+            message.put("Message", "Team at " + teamId + " was deleted successfully!");
+            return ResponseEntity.status(HttpStatus.OK).header("content-type", "application/json").body(message);
+        } else {
+            message.put("Error", "Team at " + teamId + " couldn't be deleted.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
         }
     }
@@ -67,33 +83,35 @@ public class TeamController {
     public ResponseEntity<Object> addTeam(@RequestBody Team team)
     {
         Map<String, String> message = new HashMap<String, String>();
-        Team success = ts.addTeam(team);
-        if(success == null)
-        {
-            message.put("Error", "Team was not added");
+        Optional<Team> isTeamNameTaken = Optional.ofNullable(tr.findByName(team.getTeam_name()));
+
+        if (isTeamNameTaken.isPresent()) {
+            message.put("Error", "Team name is already being used. Must be unique!");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
-        } else
-        {
-            message.put("Message", "Team added");
-            return ResponseEntity.status(200).header("content-type", "application/json").body(message);
+        } else {
+            ts.addTeam(team);
+            message.put("Message", "Team was successfully added!");
+            return ResponseEntity.status(HttpStatus.OK).header("content-type", "application/json").body(message);
         }
 
     }
 
     @PatchMapping(value = "/update", consumes = "application/json", produces ="application/json")
-    public ResponseEntity<Object> updateTeam(@RequestBody Team team)
-    {
+    public ResponseEntity<Object> updateTeam(@RequestBody Team team) {
         Map<String, String> message = new HashMap<String, String>();
-        Team success = ts.updateTeam(team);
-        if (success == null)
-        {
-            message.put("Error", "Team at " + team.getId() + " was not updated. Team may not have been added.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
-        } else
-        {
-            message.put("Message", "Team at " + team.getId() + " updated");
-            return ResponseEntity.status(200).header("content-type", "application/json").body(message);
-        }
+        Optional<Team> isTeamIdInDB = Optional.ofNullable(tr.findByTeamId(team.getId()));
+        Optional<Team> isTeamNameTaken = Optional.ofNullable(tr.findByName(team.getTeam_name()));
 
+        if (isTeamIdInDB.isEmpty()) {
+            message.put("Error", "Team does not exist in the database (incorrect team id)!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+        } else if (isTeamNameTaken.isPresent()) {
+            message.put("Error", "Team name is already being used. Must be unique!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+        } else {
+            ts.updateTeam(team);
+            message.put("Message", "Team was successfully updated!");
+            return ResponseEntity.status(HttpStatus.OK).header("content-type", "application/json").body(message);
+        }
     }
 }
